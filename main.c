b830960fe7c397d8b8b7af34b55c866b40ac1fd1
Thibaut Marty <thibaut.marty@insa-rennes.fr>
2025-01-07 12:00:00 +0100

    Polite program speaks french except if an option is given

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Hello, world!\n");
+  if(argc > 1) {
+    printf("Hello, world!\n");
+  }
+  else
+    printf("Bonjour le monde !\n");
+  }
   return 0;
 }
